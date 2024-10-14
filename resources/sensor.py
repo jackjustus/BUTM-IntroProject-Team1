3a23ffe512990d0014a90750908b80b28e2d5c75
jesseAMRamos <jesse637@bu.edu>
2024-10-14 16:45:05 +0000

    fix mistake #81

--- resources/sensor.py
+++ resources/sensor.py
@@ -66,7 +66,7 @@ class sensor_data_collector:
         za_data = self.get_data_timestamped()
         name = asctime(localtime(self.current_time)).replace(' ', '_')
         name = name.replace(':', '-')
-        with open(f'data_{name}.csv', 'w') as c:
+        with open(f'data_{name}.csv', 'w', newline= '') as c:
             writer = csv.writer(c)
             writer.writerow(za_data)
         return za_data
