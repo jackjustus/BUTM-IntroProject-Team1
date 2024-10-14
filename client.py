6ba736dd35d61330ab519062b0f454c92bd16976
Jack Justus <91996103+Jack-Justus@users.noreply.github.com>
2024-10-14 11:36:00 -0400

    updated server

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ import constants
 
 # ---------------CLIENT SETUP------------------
 laptopClient = net.TCPClient()
-laptopClient.set_server_address(constants.router_IP_ADRESS, constants.router_to_PI_PORT)
+laptopClient.set_server_address(constants.pi_IP_ADDRESS, constants.router_to_PI_PORT)
 laptopClient.connect_to_server()
 
 
